3f0d530a864d5787c628e2111de47e717cd94c44
thachln <thachln@gmail.com>
2018-04-23 08:17:49 +0700

    Translating: phase 1.

---
+++ make-patch.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+REM Delete the folder "dist"
+SET RELEASE_FOLDER=D:\MyProjects\github.com\sakai\fork-sakai12.0\release
+SET RELEASE_PATCH=D:\MyProjects\github.com\sakai\fork-sakai12.0\release-patch
+rd /S  /Q %RELEASE_PATCH%
+
+REM Call package.bat to make binary package
+REM call package.bat
+
+echo Prepare distribution for sakai 11.x
+mkdir %RELEASE_PATCH%\webapps %RELEASE_PATCH%\components
+
+copy %RELEASE_FOLDER%\webapps\admin-tools.war %RELEASE_PATCH%\webapps\
+xcopy %RELEASE_FOLDER%\components\localization-bundles %RELEASE_PATCH%\components\localization-bundles\ /S
+copy %RELEASE_FOLDER%\webapps\sakai-login-tool.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\portal.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-presence-tool.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-reset-pass.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-site-manage-link-helper.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-site-manage-participant-helper.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-site-manage-tool.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-tool-tool-su.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\sakai-signup-tool.war %RELEASE_PATCH%\webapps\
+copy %RELEASE_FOLDER%\webapps\lessonbuilder-tool.war %RELEASE_PATCH%\webapps\
+
