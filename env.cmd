aafc374a01dad90b4ba7ace5ff66c06046924086
thachln <thachln@gmail.com>
2022-11-27 19:45:50 +0700

    Build with JDK 1.8

---
+++ env.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_341
+SET PATH=%JAVA_HOME%\bin;%PATH%
--- package-library.cmd
+++ package-library.cmd
@@ -1,4 +1,9 @@
 @ECHO OFF
+
+ECHO View environment variables
+ECHO JAVA_HOME=%JAVA_HOME%
+ECHO PATH=%PATH%
+
 SET JAVA_OPTS='-server -Xms512m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m -XX:NewSize=192m -XX:MaxNewSize=384m -Djava.awt.headless=true -Dhttp.agent=Sakai -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Dsun.lang.ClassLoader.allowArraySyntax=true' 
 
 REM Should use the absolute path for folder release
