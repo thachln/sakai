3d64449d638178cff7abe1304a80862691d12597
thachln <thachln@gmail.com>
2023-07-15 21:52:53 +0700

    Fix issue to build docker image of sakai 22.3: use ENTRYPOINT [ "/opt/tomcat/bin/catalina.sh", "run" ]

--- docker/build/build.cmd
+++ docker/build/build.cmd
@@ -1,2 +1,1 @@
-REM https://github.com/thachln/sakai/releases/download/22.2.1/sakai22.2.u1_bin.tar.xz
-docker build --build-arg release=22.2 -t xlms/sakai:22.3 -f ./Dockerfile.binary .
+docker build --build-arg release=22.3 -t xlms/sakai:22.3 -f ./Dockerfile.binary .
